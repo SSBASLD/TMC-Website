17bf5089b03a27fde40319fdcd7f1dfb66c19239
William Zhong <anotherperson51@gmail.com>
2024-11-19 20:00:40 -0600

    didnt do much 👎

--- tmc-website/app/test/layout.tsx
+++ tmc-website/app/test/layout.tsx
@@ -7,11 +7,11 @@ export default function Layout({
 }>) {
     return (
         <div className={`${inter.className} bg-auto bg-slate-50 w-screen h-screen min-h-[900px] overflow-x-hidden`}>
-            <div className={`w-screen h-[10%] bg-auto bg-blue-100`}></div>
+            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black`}></div>
 
-            <div className={`w-screen h-[80%] bg-auto`}></div>
+            <div className={`w-screen h-[84%] bg-auto`}>{children}</div>
 
-            <div className={`w-screen h-[10%] bg-auto bg-red-100`}></div>
+            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black`}></div>
         </div>
     );
 }
--- tmc-website/app/test/page.tsx
+++ tmc-website/app/test/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <></>
+        <div className={`w-screen h-[10%] text-[50px] text-black`}>Hello</div>
     );
 }
