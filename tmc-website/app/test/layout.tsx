f2dde4ef8ec20f11b387fb776db9406f4e2c577f
William Zhong <anotherperson51@gmail.com>
2024-11-19 20:50:27 -0600

    idk use MUI package instead of the dumb stuff im doing

--- tmc-website/app/test/layout.tsx
+++ tmc-website/app/test/layout.tsx
@@ -11,7 +11,25 @@ export default function Layout({
 
             <div className={`w-screen h-[84%] bg-auto`}>{children}</div>
 
-            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black`}></div>
+            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black flex items-center grid grid-cols-[33%_33%_34%]`}>
+                <div>01</div>
+                <div>02</div>
+                <button className={`
+                    w-[20%]
+                    h-[80%]
+                    bg-auto
+                    bg-yellow-500
+                    flex
+                    items-center
+                    justify-center
+                    text-center
+                    place-self-center
+                    rounded-full
+                    ${inter.className}
+                `}>
+                    Next
+                </button>
+            </div>
         </div>
     );
 }
