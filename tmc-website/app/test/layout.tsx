09ffefbb76a80ced972def0ffccbdd3ab3b2b92c
William Zhong <anotherperson51@gmail.com>
2024-12-02 23:05:50 -0600

    basically did nothing

--- tmc-website/app/test/layout.tsx
+++ tmc-website/app/test/layout.tsx
@@ -1,35 +1,72 @@
+'use client'
+
 import { inter } from "@/app/ui/fonts";
+import { Card, CardActions, Button, Box, ThemeProvider, CssBaseline } from "@mui/material";
+import { blue, red, yellow } from "@mui/material/colors";
+import { theme } from "@/theme.config";
+import { addStyles, EditableMathField } from 'react-mathquill'
+import { useState } from "react";
+import { ClassNames } from "@emotion/react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function Layout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    addStyles();
+    const [latex, setLatex] = useState('\\frac{1}{\\sqrt{2}}\\cdot 2');
+
+
     return (
-        <div className={`${inter.className} bg-auto bg-slate-50 w-screen h-screen min-h-[900px] overflow-x-hidden`}>
-            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black`}></div>
+        <div className={`overflow-hidden overflow-y-hidden w-screen h-screen p-0 m-0`}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline></CssBaseline>
+                <Box sx={{ width: '100%', height: '100%' }}>
+                    <Box sx={{ backgroundColor: 'primary.main', height: '8vh', width: '100%' }} className={`border-b-[0.5vh] border-black`}></Box>
 
-            <div className={`w-screen h-[84%] bg-auto`}>{children}</div>
+                    <Box sx={{ height: '84vh' }}>
+                    </Box>
 
-            <div className={`w-screen h-[8%] bg-auto bg-cyan-700 outline outline-solid outline-black flex items-center grid grid-cols-[33%_33%_34%]`}>
-                <div>01</div>
-                <div>02</div>
-                <button className={`
-                    w-[20%]
-                    h-[80%]
-                    bg-auto
-                    bg-yellow-500
-                    flex
-                    items-center
-                    justify-center
-                    text-center
-                    place-self-center
-                    rounded-full
-                    ${inter.className}
-                `}>
-                    Next
-                </button>
-            </div>
-        </div>
+                    <Box sx={{ height: '8vh', backgroundColor: 'primary.main', width: '100%' }} className={`border-t-[0.5vh] border-black`}>
+
+                        <Box sx={{ width: '100%', height: '7vh', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute' }}>
+                            {/* <ClickAwayListener onClickAway={handleTooltipClose}>
+                                <div>
+                                    <Tooltip
+                                        onClose={handleTooltipClose}
+                                        open={open}
+                                        disableFocusListener
+                                        disableHoverListener
+                                        disableTouchListener
+                                        title="Add"
+                                        slotProps={{
+                                            popper: {
+                                                disablePortal: true,
+                                            },
+                                        }}
+                                    >
+                                        <Button onClick={handleTooltipOpen}>Click</Button>
+                                    </Tooltip>
+                                </div>
+                            </ClickAwayListener> */}
+                            /* Add tooltip to bring up the problem selection */
+
+                            <Button variant="contained" sx={{ backgroundColor: 'black', gap: '5px', maxHeight: '70%' }}>
+                                Problem 1 of 81
+                                <ExpandMoreIcon></ExpandMoreIcon>
+                            </Button>
+                        </Box>
+
+                        <Box sx={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', userSelect: 'none', paddingX: '5%', gap: '15px' }}>
+                            <Box sx={{ marginLeft: 'auto', display: 'flex', gap: '20px' }}>
+                                <Button variant='contained' sx={{ backgroundColor: 'primary.light', borderRadius: '30px' }}>Back</Button>
+                                <Button variant='contained' sx={{ backgroundColor: 'primary.light', borderRadius: '30px' }}>Next</Button>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </ThemeProvider >
+        </div >
     );
 }
---
+++ tmc-website/theme.config.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#0e7490',
+            dark: '#64748b',
+            light: '#2db6d6',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    }
+});
